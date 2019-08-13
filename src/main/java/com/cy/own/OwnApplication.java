b141870c850f8e232150895afbfabde8adc2cc13
cuiyang <cuiyangmail@163.com>
2019-08-13 17:04:38 +0800

    Initial commit

---
+++ src/main/java/com/cy/own/OwnApplication.java
@@ -0,0 +1,13 @@
+package com.cy.own;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cy/own/OwnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cy.own;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OwnApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
